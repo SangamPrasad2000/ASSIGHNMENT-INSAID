ddbd006cb143d04ba0a536043ed9d59156e9da50
Sangam Prasad <80503389+SangamPrasad2000@users.noreply.github.com>
2023-05-24 00:55:14 +0530

    contact.js

--- contact.js
+++ contact.js
@@ -1,30 +1,13 @@
-var contactForm = document.getElementById("contact-form");
 var popup = document.getElementById("popup");
-var closePopupButton = document.getElementById("close-popup");
 
-
-closePopupButton.addEventListener("click", function() {
-  hidePopup();
-  window.location.href = "index.html"; // Redirect to the home page
-});
-
-popup.addEventListener("click", function(event) {
-  if (event.target === popup) {
-    hidePopup();
-    window.location.href = "index.html"; // Redirect to the home page
-  }
-});
-
-function showPopup() {
-  popup.classList.remove("hidden");
+function showPopup(event) {
+  event.preventDefault(); // Prevent form submission
+  popup.classList.add("show");
 }
 
-function hidePopup() {
-  popup.classList.add("hidden");
+function closePopup() {
+  popup.classList.remove("show");
+  setTimeout(function() {
+    window.location.href = "index.html"; // Redirect to index.html after 5 seconds
+  });
 }
-
-function resetForm() {
-  contactForm.reset();
-}
-
-
